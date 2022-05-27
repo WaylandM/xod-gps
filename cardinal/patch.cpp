1b2d1481cae497099c7279f696ed419e85667ff5
WaylandM <mw283@cam.ac.uk>
2022-05-27 12:17:23 +0100

    modified:   cardinal/patch.cpp 	modified:   distance-between/patch.xodp 	modified:   project.xod

--- cardinal/patch.cpp
+++ cardinal/patch.cpp
@@ -3,14 +3,14 @@
 #include <TinyGPSPlus.h>
 
 node {
-    CStringView view;
-    char* cString;
+    CStringView view = CStringView(view);
+    char cString[4];
+    
     void evaluate(Context ctx) {
         double course = getValue<input_Course>(ctx);
         //cardinal is a static method, so we don't need an instance of the class TinyGPSPlus
         String str = TinyGPSPlus::cardinal(course);
         int strLen = str.length() + 1;
-        cString = new char[strLen];
         str.toCharArray(cString, strLen);
         view=CStringView(cString);
         emitValue<output_Card>(ctx, XString(&view));
